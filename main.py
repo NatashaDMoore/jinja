e180ef716dbe9f13bb3f57f26a0a2715d37afc9e
swista <you@example.com>
2024-02-14 23:57:34 +0000

    adds users

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, url_for
 
-#create a secret key for security
+# To create a secret key
 import os
+
+# Using JSON for data
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +16,7 @@ app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY',
                                           'default_secret_key')
 
 
+#=================== ROUTES ===================#
 @app.route('/')
 def index():
   title = "Home"
@@ -23,59 +27,6 @@ def index():
 def about():
   title = "About"
   return render_template("about.html", title=title)
-
-
-# Route for the form page
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-  title = "Register"
-  feedback = None
-  if request.method == 'POST':
-    feedback = register_data(request.form)
-
-  context = {"title": title, "feedback": feedback}
-  return render_template('register.html', **context)
-
-
-# Replace _ with space,
-# Title case key and value, excludes email and bio from title case
-# Lists all hobbies selected
-def register_data(form_data):
-  return [
-      f"{key.replace('_', ' ').replace('[]', '').title()}: {', '.join(map(str, form_data.getlist(key))).title()}"
-      if key == 'hobbies[]' else
-      f"{key.replace('_', ' ').title()}: {value.title()}" if key.lower()
-      not in ['email', 'bio'] else f"{key.replace('_', ' ').title()}: {value}"
-      for key, value in form_data.items()
-  ]
-
-
-# ---- old code -----
-# def register_data(form_data):
-#   feedback = []
-
-#   for key, value in form_data.items():
-#     if key == 'hobbies[]':
-#       # Handle hobbies as a list
-#       hobbies = form_data.getlist(key)
-#       feedback.append(
-#           f"{key.replace('_', ' ').replace('[]', '')}: {', '.join(map(str, hobbies))}"
-#       )
-#     else:
-#       feedback.append(f"{key.replace('_', ' ')}: {value}")
-
-#   return feedback
-
-# used list comprehension to simplify code
-# def register_data(form_data):
-#   return [f"{key}: {value}" for key, value in form_data.items()]
-
-# ---- old code -----
-# def register_data(form_data):
-#   feedback = []
-#   for key, value in form_data.items():
-#     feedback.append(f"{key}: {value}")
-#   return feedback
 
 
 @app.route('/hipster')
@@ -90,7 +41,7 @@ def coffee():
   return render_template("coffee.html", title=title)
 
 
-#----- Movie dictionary -----
+#----- Movie dictionary -----#
 movie_dict = [{
     "title": "Napoleon",
     "genre": "Drama",
@@ -123,6 +74,64 @@ def movies():
   context = {"title": "Movies", "movies": movie_dict}
 
   return render_template("movies.html", **context)
+
+
+# Route for the form page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  title = "Register"
+  feedback = None
+  if request.method == 'POST':
+    feedback = register_data(request.form)
+
+  context = {"title": title, "feedback": feedback}
+  return render_template('register.html', **context)
+
+
+# Replace _ with space,
+# Title case key and value, excludes email and bio from title case
+# Lists all hobbies selected
+def register_data(form_data):
+  return [
+      f"{key.replace('_', ' ').replace('[]', '').title()}: {', '.join(map(str, form_data.getlist(key))).title()}"
+      if key == 'hobbies[]' else
+      f"{key.replace('_', ' ').title()}: {value.title()}" if key.lower()
+      not in ['email', 'bio'] else f"{key.replace('_', ' ').title()}: {value}"
+      for key, value in form_data.items()
+  ]
+
+
+# ---- old code -----
+# used list comprehension to simplify code
+# def register_data(form_data):
+#   return [f"{key}: {value}" for key, value in form_data.items()]
+
+# ---- old code -----
+# def register_data(form_data):
+#   feedback = []
+#   for key, value in form_data.items():
+#     feedback.append(f"{key}: {value}")
+#   return feedback
+
+
+# Route for the users page
+@app.route('/users')
+def users():
+
+  # Read project data from JSON file (test.json)
+  with open('test.json') as json_file:
+    user_data = json.load(json_file)
+    print(user_data)
+
+  context = {"title": "Users", "users": user_data}
+
+  return render_template("users.html", **context)
+
+
+# View page for each user
+@app.route('/user/<int:user_number>')
+def user(user_number):
+  return f'Content for User {user_number}'
 
 
 app.run(host='0.0.0.0', port=81)
